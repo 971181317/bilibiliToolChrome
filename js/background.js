3f1451b7f1fb5ccd21de4c8ee47dd5c593881c5d
xingyu duan <duanxingyu@bytedance.com>
2021-03-12 12:04:15 +0800

    add

--- js/background.js
+++ js/background.js
@@ -5,6 +5,7 @@
 //初始化
 (function () {
     init()
+    setDark()
 })()
 
 function init() {
@@ -16,13 +17,27 @@ function init() {
 }
 
 function setDark(isDark) {
-    if (isDark == "false") {
-        localStorage.setItem("isDark", "false")
-    } else {
-        localStorage.setItem("isDark", "true")
-        chrome.tabs.executeScript({code: 'bilibiliDarkStart()'});
-    }
+    chrome.tabs.getSelected(function (tab) {
+        if (tab.url.indexOf("bilibili.com") > -1) {
+            if (isDark == "true") {
+                localStorage.setItem("isDark", "false")
+                //建立监听器
+                chrome.tabs.onCreated.addListener(null)
+                chrome.tabs.onUpdate.addListener(null)
+            } else {
+                localStorage.setItem("isDark", "true")
+                //建立监听器
+                chrome.tabs.onCreated.addListener(function (tab) {
+                    chrome.tabs.executeScript({code: 'bilibiliDarkStart()'});
+                })
+                chrome.tabs.onUpdate.addListener(function (tabId, changeInfo, tab) {
+                    tab.executeScript({code: 'bilibiliDarkStart()'});
+                })
+            }
+        }
+    })
 }
+
 function getDark() {
     return localStorage.getItem('isDark')
 }
